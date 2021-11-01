38d4d10b0046165fc4ebdb3ce8e7a6ce609f239d
boshan200 <boshan200@gmail.com>
2021-11-01 17:41:33 +0800

    修改遠端分支後重新push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App HAHAHA"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App HEHEHEHE"/>
   </div>
 </template>
 
